860cf39454f9595bfbf3eafa404eb30877b2fbb1
Xave-X <38805948+Xave-X@users.noreply.github.com>
2022-10-09 16:50:30 +0100

    Created page for adding new book entries

---
+++ src/resources/views/addBook.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+    </head>
+    <body>
+
+        <div class="flex-center position-ref full-height">
+            <h1>Add book</h1>
+        </div>
+
+        <div class="newRecord">
+            <div class="new">
+                <span class="label label-default">New Book</span>
+            </div>
+            <form method="POST" action="{{ url('/main/newBook')}}">
+                {{ csrf_field() }}
+                <div class="flex form-group">
+                    <label for="usr">Title:</label>
+                    <input type="text" class="form-control" name="bookTitle">
+                </div>
+                <div class="flex form-group">
+                    <label for="usr">Author:</label>
+                    <input type="text" class="form-control" name="bookAuthor">
+                </div>
+                <button type="submit" class="btn btn-success">Add</button>
+            </form>
+        </div>
+    </body>
+</html>
