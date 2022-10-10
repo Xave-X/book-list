4bf57e42f756ed5ae21e519b4fc325081c7c643b
Xave-X <38805948+Xave-X@users.noreply.github.com>
2022-10-10 01:07:58 +0100

    Updated routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -17,4 +17,26 @@
 
 Route::get('/', 'BooksController@books'); #at localhost/, we go to the controller...
 #...and the controller returns the view 'welcome'
+Route::get('/booksByAuthor', 'BooksController@booksByAuthor');
+
+Route::get('/booksByTitle', 'BooksController@booksByTitle');
+
 Route::get('/deleteBook', 'BooksController@deleteBook');
+
+Route::get('/addBookPage', 'BooksController@addBookPage');
+
+Route::post('/addBook', 'BooksController@addBook'); #see newBook in 'other' controller
+
+Route::get('/editBookPage', 'BooksController@editBookPage');#get? post?
+
+Route::post('/editBook', 'BooksController@editBook');
+
+Route::post('/search', 'BooksController@search');#get? post?
+
+Route::get('/export', 'BooksController@export');
+Route::get('/exportCSVTA', 'BooksController@exportCSVTA');
+Route::get('/exportCSVTitles', 'BooksController@exportCSVTitles');
+Route::get('/exportCSVAuthors', 'BooksController@exportCSVAuthors');
+Route::get('/exportXMLTA', 'BooksController@exportXMLTA');
+Route::get('/exportXMLTitles', 'BooksController@exportXMLTitles');
+Route::get('/exportXMLAuthors', 'BooksController@exportXMLAuthors');
