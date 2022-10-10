cad0608cd93fbfa1039bf5af54a061bd9f26a063
Xave-X <38805948+Xave-X@users.noreply.github.com>
2022-10-10 01:07:14 +0100

    Edited welcome view to add buttons for searching/adding/sorting/editing/exporting books

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -11,6 +11,26 @@
             <h1>Book list</h1>
         </div>
 
+        <form action="{{ url('/search')}}" method="POST" role="search">
+            {{ csrf_field() }}
+            <div class="input-group">
+                <input type="text" class="form-control" name="q"
+                    placeholder="Search by title or author">
+                    <span class="input-group-btn">
+                        <button type="submit" class="btn btn-default">
+                            <span>Search</span>
+                        </button>
+                    </span>
+            </div>
+        </form>
+
+
+        <a href="{{action('BooksController@addBookPage')}}">Add</a>
+        <a href="{{action('BooksController@export')}}">Export</a>
+        <a href="{{action('BooksController@booksByAuthor')}}">Sort by author</a>
+        <a href="{{action('BooksController@booksByTitle')}}">Sort by title</a>
+
+
         <div class="table-wrapper-scroll-y">
             <table class="table table-bordered table-striped">
                 <thead>
@@ -37,8 +57,8 @@
                         Then, 'id' is passed to the controller. In the controller, look at the 
                         $id = $request->id; line. In that line, the id before the ; is the 'id'
                         bit in the construction above. -->
-                        <td>{{ $book->genreName }}</td>
-                        <td>{{ $book->numOfPages }}</td>
+                        <td><a href="{{action('BooksController@editBookPage', ['id' => $book -> id])}}">Edit</a></td>
+
                     </tr>
                     @php ($i= $i + 1)
                 @endforeach
